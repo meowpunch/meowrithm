8b768350aef02ff0d1dacd86f6df0f4dd3d4f800
meowpunch <jinhoonbae@gmail.com>
2019-04-11 13:56:04 +0900

    '04.11'

--- 1260.cpp
+++ 1260.cpp
@@ -1,31 +1,97 @@
-//DFS BFS   수정 깃
-
 #include <iostream>
+#include <vector>
 #include <stack>
 #include <queue>
+using namespace std;
 
-int map[1001][1001];
+vector<int> dfsD;
+vector<int> bfsD;
+int a[1002][1002]={0,};
+int ch[1002]={0,};
+int ch2[1002]={0,};
+//int count=0;
+
+void dfs(int S, int N, int M){
+
+   stack<int> stc;
+
+    stc.push(S);
+    int tmp;
+
+    while(!stc.empty()){
+    //  if(count==N) break;
+      int top = stc.top();
+      if(ch[top]==0){
+
+            dfsD.push_back(top);
+            ch[top]=1;
+            //printf("%d ",stc.top());
+            stc.pop();
+
+            for(int i=N; i>0; i--){
+                if(a[top][i]==1  && ch[i]==0)
+                stc.push(i);
+            }
+      } else {
+         stc.pop();
+      }
+    }
+}
+
+void bfs(int S, int N){
+   int i, here;
+   //ch2[S]=1;
+   queue<int> que;
+   que.push(S);
+   //int count[N+1]={0,};
+
+   while(!que.empty())
+   {
+      int front = que.front();
+      if(ch2[front]==0){
+           ch2[front]=1;
+           //("%d", que.front());
+           bfsD.push_back(front);
+           que.pop();
+
+          for(i=1; i<=N; i++)
+          {
+             //printf("%d", que.front());
+             if(a[i][front]==1 && ch2[i]==0){
+                 que.push(i);
+             }
+           }
+
+      } else {
+         que.pop();
+      }
 
 
-vector<int> DFS() {
-
+   }
 
 }
 
 int main() {
+   int N, M, S, i;
+   scanf("%d %d %d", &N, &M, &S);
 
-  int N, M, V;
-
-  scnaf("%d %d %d", &N, &M, &V);
-
-  int i;
-  for(i=0; i<M; i++) {
-    int from, to, weight;
-    map[from][to] = weight; map[to][from] = weight;
-  }
-
-  DFS(V);
+   for(i=0; i<M; i++)
+   {int b, c;
+      scanf("%d %d", &b, &c);
+      a[b][c]=a[c][b]=1;
+      //if(i>0) a[i][i]=1;
+   }
 
 
-  return 0;
+    dfs(S, N, M);
+    for(i=0; i<dfsD.size(); i++)
+      printf("%d ", dfsD[i]);
+
+   printf("\n");
+
+    bfs(S, N);
+   for(i=0; i<bfsD.size(); i++)
+      printf("%d ", bfsD[i]);
+
+   return 0;
 }
--- 1753.cpp
+++ 1753.cpp
@@ -7,24 +7,41 @@ using namespace std;
 
 vector< pair<int, int>> map[20001];  //도착정점과 간선 길이
 
-
 vector<int> Dijkstra(int S, int V, int E) {
 
-    vector<int> dist(V, INT_MAX);
+    vector<int> dist(V+1, INT_MAX);
+    vector<int> visit(V+1, 0);
     dist[S] = 0;
 
-    priority_queue< pair<int, int>> pq; //간선 길이
+    priority_queue<pair<int, int>> pq; //간선 길이, 도착 정점
 
     pq.push(make_pair(0, S));
 
-    while(pq.empty)
+    while(!(pq.empty())) {
+        int weight = pq.top().first; //가장 짧은 길이 선택
+        int top = pq.top().second;
 
+        if(visit[top] == 0){  //방문안했다면 방문.
+            //printf("visit %d\n", top);
+            pq.pop();
+            visit[top] = 1;
+            //printf("hi");
+            for(int i=0; i<map[top].size(); i++) {
+                int to = map[top][i].first;
+                //printf("connected: %d \n", to);
+                int edge = map[top][i].second;
 
+                if(dist[to] > dist[top] + edge) dist[to] = dist[top] + edge;
 
+                pq.push(make_pair(-edge, to));
+                //printf("push %d \n", to);
+            }
+        } else {
+            pq.pop();
+        }
+    }
+    return dist;
 }
-
-
-
 
 int main() {
 
@@ -34,18 +51,18 @@ int main() {
     int start;
     scanf("%d", &start);
 
-    int i,j;
-
-
-    for(i=0; i<Enum; i++) {
+    for(int i=0; i<Enum; i++) {
         int from, to, weight;
-        scanf("%d %d %d", &from, &to, weight);
+        scanf("%d %d %d", &from, &to, &weight);
 
         map[from].push_back(make_pair(to,weight));
     }
 
     vector<int> ans = Dijkstra(start, Vnum, Enum);
 
+    for(int i=1; i<ans.size(); i++){
+        if(ans[i]<INT_MAX) printf("%d\n", ans[i]);
+        else printf("INF\n");
+    }
     return 0;
 }
-
