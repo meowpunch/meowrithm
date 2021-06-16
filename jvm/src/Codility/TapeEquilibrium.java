7f035ebe5db0a7f926abc5b492f742b1e9161d86
meowpunch <jinhoonbae@gmail.com>
2021-06-16 02:37:56 +0200

    JB: array

--- jvm/src/Codility/TapeEquilibrium.java
+++ jvm/src/Codility/TapeEquilibrium.java
@@ -3,22 +3,26 @@ package Codility;
 public class TapeEquilibrium {
     /**
      *   let N be the length of A
+     *   Any integer P, such that 0 < P < N, splits this tape into two non-empty parts
      *   - N is an integer within the range [2..100,000];
      *   - each element of array A is an integer within the range [-1,000..1,000]
+     *
+     *   Time/Space: O(N)/O(1)
      */
     public int solution(int[] A) {
 
-        long right = summation(A);
-        long left = 0;
-        long result = absolute(right);
+        // init - O(N)
+        long right = summation(A) - A[0];
+        long left = A[0];
+        long result = absolute(right - left);
 
-        for (int a: A) {
-            left += a;
-            right -= a;
+        // dp - O(N)
+        for (int p = 2; p < A.length; p++) {
+            left += A[p - 1];
+            right -= A[p - 1];
 
             final long diff = absolute(right - left);
             if (diff < result) result = diff;
-            else break;
         }
 
         return (int) result;
@@ -27,7 +31,7 @@ public class TapeEquilibrium {
 
     private long summation(int[] A) {
         long result = 0;
-        // aggreate elements
+        // aggregate elements
         for (int a: A) result += a;
         return result;
     }
