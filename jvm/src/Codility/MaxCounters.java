76a2d15efc8645d5052ddd6cd90bfc156be78565
meowpunch <jinhoonbae@gmail.com>
2021-06-16 10:44:02 +0200

    JB: count elents

--- jvm/src/Codility/MaxCounters.java
+++ jvm/src/Codility/MaxCounters.java
@@ -3,14 +3,43 @@ package Codility;
 import java.util.Arrays;
 
 public class MaxCounters {
+
+    /**
+     *  if we know the point of max counter, we only need to know the maximum value at that point.
+     */
+    public int[] solution(int N, int[] A) {
+        // init
+        int[] counts = new int[N];
+
+        // let's count - O(M)
+        int maxCount = 0;
+        int startingLine = 0;
+        for (int x: A) {
+            if (x == N+1) {
+                startingLine = maxCount;
+            } else {
+                counts[x - 1] = Math.max(counts[x - 1] + 1, startingLine + 1);
+                maxCount = Math.max(maxCount, counts[x - 1]);
+            }
+        }
+
+        // post adjust - O(N)
+        for (int i = 0; i < N; i++) {
+            counts[i] = Math.max(startingLine, counts[i]);
+        }
+
+        return counts;
+    }
+
+
     // let the length of A be M
     //
     // * prerequisites
     //  N and M are integers within the range [1..100,000];
     //  each element of array A is an integer within the range [1..N + 1].
     //
-    //
-    public int[] solution(int N, int[] A) {
+    //  Time, Space: O(n^2), O(n)
+    public int[] solution2(int N, int[] A) {
         final int M = A.length;
 
         int[] counters = new int[N];
