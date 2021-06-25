5614cb3a979c626660923357ba8ab79c8ed115ad
meowpunch <jinhoonbae@gmail.com>
2021-06-25 13:28:05 +0200

    JB: Zalando Codility

--- jvm/src/Others/Test.java
+++ jvm/src/Others/Test.java
@@ -8,39 +8,11 @@ public class Test {
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3)));
+        int a = 11;
 
-        ReversedLinkedList solution = new ReversedLinkedList();
-        ListNode reversed = solution.reverseList(head);
+        System.out.println(11/2);
+        System.out.println(11/3);
 
-        while (reversed != null) {
-            System.out.print(reversed.val + " ");
-            reversed = reversed.next;
-        }
-
-        Integer a = 0;
-        new Test().func1(a);
-        System.out.println(a);
-
-        Integer x = new Integer(0);
-        Integer y = x;
-        Integer z = y;
-        x = 3;
-
-        System.out.printf("%s %s %s\n", x, y, z);
-        System.out.printf("%s %s %s\n\n", x.hashCode(), y.hashCode(), z.hashCode());
-
-        A obj1 = new A();
-        A obj2 = obj1;
-        obj1 = new A();
-        obj1.a = 3;
-
-        System.out.printf("%s %s\n", obj1.a, obj2.a);
-        System.out.printf("%s %s\n", obj1.hashCode(), obj2.hashCode());
-
-
-        String s = "123";
-        int occurrence = 1;
     }
 }
 
