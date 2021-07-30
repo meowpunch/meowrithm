2b41c7910bd703cbcaa0719d95e8bffdc5d0c618
meowpunch <jinhoonbae@gmail.com>
2021-07-30 16:33:42 +0200

    JB: text matching

--- jvm/src/Amazon/FreshPromo.java
+++ jvm/src/Amazon/FreshPromo.java
@@ -1,36 +1,81 @@
 package Amazon;
-import java.util.List;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+/**
+ * https://leetcode.com/discuss/interview-question/1002811/Amazon-or-OA-20201-or-Fresh-Promotion
+ */
 public class FreshPromo {
+    public static void main(String[] args) {
+        Stream.of(
+                FreshPromo.foo(
+                        Arrays.asList(
+                                Arrays.asList("apple", "apple"),
+                                Arrays.asList("banana", "anything", "banana")
+                        ),
+                        Arrays.asList(
+                                "apple", "apple", "apple", "banana", "orange", "banana"
+                        )
+                ),
+                FreshPromo.foo(
+                        Arrays.asList(
+                                Arrays.asList("apple", "apple"),
+                                Arrays.asList("banana", "anything", "banana")
+                        ),
+                        Arrays.asList(
+                                "apple", "apple", "apple", "banana", "banana", "apple", "banana"
+                        )
+                ),
+                FreshPromo.foo(
+                        Arrays.asList(
+                                Arrays.asList("apple", "apple"),
+                                Arrays.asList("banana", "anything", "banana")
+                        ),
+                        Arrays.asList(
+                                "apple", "apple", "apple", "banana", "banana", "apple"
+                        )
+                )
+        ).forEach(System.out::println);
+
+    }
 
     /**
-     * @param codeList [[apple, apple], [banana, anything, banana]]
+     * we can enhance this method with KMP algorithm
+     *
+     * @param codeList     [[apple, apple], [banana, anything, banana]]
      * @param shoppingCart [orange, apple, apple, banana, orange, banana]
      * @return 1(match) or 0(not)
      */
     public static int foo(List<List<String>> codeList, List<String> shoppingCart) {
         final int cartSize = shoppingCart.size();
+        final int codeListSize = codeList.size();
         int cartIndex = 0;
         int codeIndex = 0;
 
-        while(cartIndex < cartSize) {
+        while (cartIndex < cartSize && codeIndex < codeListSize) {
+            final String curItem = shoppingCart.get(cartIndex);
+            final List<String> curCode = codeList.get(codeIndex);
 
-            List<String> code = codeList.get(codeIndex);
-
-            // check if code in shoppingCart
-            for (String item: code) {
-                if (!item.equals("anything")) {
-                    final int index = shoppingCart.subList(cartIndex, cartSize + 1).indexOf(item);
-                    if (index > 0) cartIndex = index + 1;
-                    else return 0;
-                }
-
-                cartIndex++;
+            if ((curCode.get(0).equals(curItem) || curCode.get(0).equals("anything")) &&
+                    (cartIndex + curCode.size() <= cartSize && checkMatching(shoppingCart.subList(cartIndex, cartIndex + curCode.size()), curCode))) {
+                cartIndex += curCode.size();
+                codeIndex += 1;
+            } else {
+                cartIndex += 1;
             }
-
-
-            // increment index
         }
-        return 1;
+        return codeIndex == codeListSize ? 1 : 0;
+    }
+
+    private static boolean checkMatching(List<String> cart, List<String> code) {
+//        System.out.printf("%s %s\n", cart, code);
+        for (int i = 0; i < cart.size(); i++) {
+            if (!code.get(i).equals("anything") && !code.get(i).equals(cart.get(i))) return false;
+        }
+        return true;
     }
 }
---
+++ jvm/src/Amazon/OptimalUtilization.java
@@ -0,0 +1,13 @@
+package Amazon;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/discuss/interview-question/373202
+ */
+public class OptimalUtilization {
+
+    public static List<List<Integer>> optimalUtilization(List<List<Integer>> A, List<List<Integer>> B, int target) {
+        throw new UnsupportedOperationException();
+    }
+}
