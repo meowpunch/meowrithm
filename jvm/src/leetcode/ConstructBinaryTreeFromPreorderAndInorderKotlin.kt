326eb5c61b74e9d9be8833c45990455ca40aed8d
meowpunch <jinhoonbae@gmail.com>
2021-10-06 12:09:39 +0200

    JB: pratice kotlin

--- jvm/src/leetcode/ConstructBinaryTreeFromPreorderAndInorderKotlin.kt
+++ jvm/src/leetcode/ConstructBinaryTreeFromPreorderAndInorderKotlin.kt
@@ -1,6 +1,16 @@
 package leetcode
 
 import data.TreeNode
+import java.util.*
+
+fun main() {
+    println(
+        ConstructBinaryTreeFromPreorderAndInorderKotlin().buildTree(
+            listOf(3, 9, 20, 15, 7).toIntArray(),
+            listOf(9, 3, 15, 20, 7).toIntArray()
+        )
+    )
+}
 
 class ConstructBinaryTreeFromPreorderAndInorderKotlin {
 
@@ -12,7 +22,7 @@ class ConstructBinaryTreeFromPreorderAndInorderKotlin {
                      1 2        5
 
             inorder: left -> root -> right  e.g. [9, 1, 7, 2, 3, 8, 20, 13, 5]
-            preorder: root -> left -> right e.g. [3, 9, 20, 7, 8, 13, 1, 2, 5]
+            preorder: root -> left -> right e.g. [3, 9, 7, 1, 2, 20, 8, 13, 5]
 
 
             root: preorder[0] => 3
@@ -20,6 +30,27 @@ class ConstructBinaryTreeFromPreorderAndInorderKotlin {
             root 9                    root 20
             LST [] RST[1, 7, 2]       LST [8] RST [13, 5]
         */
+        //
+//        val inorderIndexToValue = inorder.mapIndexed { index, value -> value to index }.toMap()
 
+        fun build(preorder: IntArray, inorder: IntArray): TreeNode? {
+            if (preorder.isEmpty()) return null
+            else {
+                val rootVal = preorder[0]
+                val rootNode = TreeNode(rootVal)
+                val inorderRootIdx = inorder.indexOf(rootVal)
+
+                rootNode.left =
+                    build(preorder.copyOfRange(1, inorderRootIdx + 1), inorder.copyOfRange(0, inorderRootIdx))
+                rootNode.right = build(
+                    preorder.copyOfRange(inorderRootIdx + 1, preorder.size),
+                    inorder.copyOfRange(inorderRootIdx + 1, inorder.size)
+                )
+                return rootNode
+            }
+        }
+
+        return build(preorder, inorder)
     }
+
 }
