88d3f49f0e80ca415e2c361197455a583c7243fe
meowpunch <jinhoonbae@gmail.com>
2021-12-10 09:53:56 +0100

    first qunique character in a string with java

---
+++ java-kotlin/src/leetcode/FirstUniqueCharacterInString.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class FirstUniqueCharacterInString {
+
+    /*
+        time/space O(N)/O(1)
+        int Array is better than HashMap
+
+        space 26, # of alphabet characters
+     */
+    public int firstUniqChar(String s) {
+        final int[] count = new int[26];
+//        Arrays.fill(count, 0);
+
+        // N
+        for (char c : s.toCharArray()) count[c - 'a'] += 1;
+
+        // N
+        for (int i = 0; i < s.length(); i++) if (count[s.charAt(i) - 'a'] == 1) return i;
+
+        return -1;
+    }
+}
