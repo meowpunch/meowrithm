5cd0273ff6b1ae5ee83fbc757f2e0ee45b513565
meowpunch <jinhoonbae@gmail.com>
2021-12-30 23:28:13 +0100

    correct the wrong solution of First Unique

--- java-kotlin/src/company/sap/FirstUnique.java
+++ java-kotlin/src/company/sap/FirstUnique.java
@@ -4,25 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FirstUnique {
+    public static void main(String[] args) {
+        System.out.println(new FirstUnique().firstUnique("supercalifragilisticexpialidocious"));
+    }
 
     /*
         input: supercalifragilisticexpialidocious
         output: f
     */
-
     public Character firstUnique(String input) {
-        // Char -> whether is unique or not
-        final Map<Character, Boolean> m = new HashMap<>();
+        // the number of occurrence of character
+        final int[] o = new int[26];
 
-        for (char c: input.toCharArray()) {
-            if(m.getOrDefault(c, false)) {
-                m.put(c, false);
-            } else m.put(c, true);
-        }
+        for (char c: input.toCharArray()) o[c - 'a'] += 1;
 
-        for (char c: input.toCharArray()) {
-            if(m.getOrDefault(c, false)) return c;
-        }
+        for (char c: input.toCharArray()) if(o[c - 'a'] == 1) return c;
+
+        return null;
+    }
+
+    public Character firstUniqueWithHashMap(String input) {
+        // Char -> the number of occurrence of character
+        final Map<Character, Integer> m = new HashMap<>();
+
+        for (char c: input.toCharArray()) m.put(c, m.getOrDefault(c, 0) + 1);
+
+        for (char c: input.toCharArray()) if(m.getOrDefault(c, 0) == 1) return c;
 
         return null;
     }
