0021a364ab7d009b2ffe313992448927c99028bf
meowpunch <jinhoonbae@gmail.com>
2023-07-31 20:51:09 +0200

    Page manipulation

---
+++ java-kotlin/src/company/deliveryhero/PageManipulation.java
@@ -0,0 +1,39 @@
+package company.deliveryhero;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class PageManipulation {
+  private record PageData(String url, String title, List<String> links) {}
+
+  public static List<PageData> removeDuplicateLinks(List<PageData> pages) {
+    return pages.stream().map(PageManipulation::removeDuplicateLinks).toList();
+  }
+
+  /*
+    let N be links's length
+
+    V time O(N) / space O(N)
+    HashSet<String>
+      if set contains the link, it's already appeared.
+
+    time O(NlogN)/ space O(1)
+    sorting
+  */
+  public static PageData removeDuplicateLinks(PageData page) {
+    var set = new HashSet<>(page.links);
+    return new PageData(page.url, page.title, set.stream().toList());
+  }
+
+  public static void main(String[] args) {
+    ArrayList<String> strings = new ArrayList<String>();
+    strings.add("Hello, World!");
+    strings.add("Welcome to CoderPad.");
+    strings.add("This pad is running Java " + Runtime.version().feature());
+
+    for (String string : strings) {
+      System.out.println(string);
+    }
+  }
+}
